df2f7039c24cb801ee359ee46c12d262d2dfd0c8
Your Name <your@email.com>
2025-08-19 09:36:43 +0800

    Initial commit: ASP.NET MVC project

---
+++ GradingPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GradingPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
